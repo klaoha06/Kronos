27c8ff98193df8455c5238eeb9fb614bd21fa566
Supakorn Laohasongkram <laohasongkram.supakorn@gmail.com>
2014-11-23 15:37:49 -0800

    react main page layout

---
+++ public/js/app.js
@@ -0,0 +1,162 @@
+var Container = React.createClass({
+  render: function() {
+            return (
+              <div id="container">
+                <Navbar />
+                <div id="main-container" className="row gutters">
+                  <Sidebar />
+                  <MainPanel />
+                </div>
+              </div>
+            );
+          }
+});
+
+var Navbar = React.createClass({
+  render: function() {
+            return (
+              <div id="navbar">
+                <input id="search" type="text" />
+                <div id="console">
+                  <a href="#">Sign In</a>
+                </div>
+              </div>
+            );
+          }
+});
+
+var Sidebar = React.createClass({
+  render: function() {
+            return (
+              <div id="sidebar" className="debug col span_2">
+                <div id="myCalendars"></div>
+                <div id="popularCalendars"></div>
+              </div>
+            );
+          }
+});
+
+var MainPanel = React.createClass({
+  getInitialState: function() {
+                     return { view: 'week' };
+                   },
+
+  changeView: function(view) {
+                this.setState({ view: view });
+              },
+
+  render: function() {
+            return (
+              <div id="main-panel" className="debug col span_10">
+                <ViewOptions changeView={this.changeView} />
+                <CalendarView view={this.state.view} />
+              </div>
+            );
+          }
+});
+
+var ViewOptions = React.createClass({
+  handleClick: function(e) {
+                var view = e.currentTarget.id;
+                this.props.changeView(view);
+               },
+
+  render: function() {
+            return (
+              <div id="view-options" className="debug cf">
+                <div id="pagination">
+                  <button id="now">Now</button>
+                  <button id="last">Last</button>
+                  <button id="next">Next</button>
+                </div>
+                <div id="view-buttons">
+                  <button id="day" onClick={this.handleClick}>Day</button>
+                  <button id="week" onClick={this.handleClick}>Week</button>
+                  <button id="month" onClick={this.handleClick}>Month</button>
+                  <button id="year" onClick={this.handleClick}>Year</button>
+                </div>
+              </div>
+            );
+          }
+});
+
+var CalendarView = React.createClass({
+  render: function() {
+            if (this.props.view == 'week') {
+              return (
+                <div id="calendar-view">
+                  <WeekView />
+                </div>
+              );
+            } else if (this.props.view == 'month') {
+              return (
+                <div id="calendar-view">
+                  <MonthView />
+                </div>
+              );
+            }
+          }
+});
+
+var WeekView = React.createClass({
+  render: function() {
+            return (
+              <div id="week-view">
+                <div id="monday" className="weekday"></div>
+                <div id="tuesday" className="weekday"></div>
+                <div id="wednesday" className="weekday"></div>
+                <div id="thursday" className="weekday"></div>
+                <div id="friday" className="weekday"></div>
+                <div id="saturday" className="weekday"></div>
+                <div id="sunday" className="weekday"></div>
+              </div>
+            );
+          }
+});
+
+var MonthView = React.createClass({
+  render: function() {
+            return (
+              <div id="month-view">
+                <div className="week rows">
+                  <div className="day"></div>
+                  <div className="day"></div>
+                  <div className="day"></div>
+                  <div className="day"></div>
+                  <div className="day"></div>
+                  <div className="day"></div>
+                  <div className="day"></div>
+                </div>
+                <div className="week rows">
+                  <div className="day"></div>
+                  <div className="day"></div>
+                  <div className="day"></div>
+                  <div className="day"></div>
+                  <div className="day"></div>
+                  <div className="day"></div>
+                  <div className="day"></div>
+                </div>
+                <div className="week rows">
+                  <div className="day"></div>
+                  <div className="day"></div>
+                  <div className="day"></div>
+                  <div className="day"></div>
+                  <div className="day"></div>
+                  <div className="day"></div>
+                  <div className="day"></div>
+                </div>
+                <div className="week rows">
+                  <div className="day"></div>
+                  <div className="day"></div>
+                  <div className="day"></div>
+                  <div className="day"></div>
+                  <div className="day"></div>
+                  <div className="day"></div>
+                  <div className="day"></div>
+                </div>
+              </div>
+            );
+          }
+});
+
+React.render(<Container />, document.body);
