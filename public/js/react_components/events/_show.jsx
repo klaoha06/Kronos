6b561bfa130a9e647a440c20dc2cc47151d9ed6b
Francis John <francis.john@colorado.edu>
2014-12-21 18:40:23 -0800

    disply something for event show page

--- public/js/react_components/events/_show.jsx
+++ public/js/react_components/events/_show.jsx
@@ -1,6 +1,15 @@
 define(['react', 'jquery', 'react-router', 'serverSetup', 'actions/EventViewActions', 'moment'], function(React, $, Router, api, EventViewActions, moment){
 
+	var Event = React.createClass({
+		mixins: [Router.State], //Gives us ability to check URL with this.getParams
+		render: function(){
+			return(
+				<h1>Event Show Page for Event # {this.getParams().id} </h1>
+				)
+		}
+	})
 
+	return Event;
 
 });
 	
