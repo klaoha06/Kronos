535becfe30772b861f2afaaae24bd57b2fdbc0cd
klaoha06 <laohasongkram.supakorn@gmail.com>
2015-01-06 21:37:35 +0700

    initial

---
+++ public/js/modal.js
@@ -0,0 +1,7 @@
+define(['react', 'jquery', 'jquery-ui-custom', 'react-router','serverSetup'], function(React, $, jqueryUI, Router, api, CalendarStore, CalendarActions, CalendarAPI, Foundation, Reveal){
+	var ModalWindow = function(){
+		
+	};
+
+	return ModalWindow;
+});
