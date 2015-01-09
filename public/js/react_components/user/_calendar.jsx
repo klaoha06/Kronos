0f76c3a671116411f66c1bbd5c7ad7232944ad88
klaoha06 <laohasongkram.supakorn@gmail.com>
2015-01-09 16:01:34 +0700

    minor fix

--- public/js/react_components/user/_calendar.jsx
+++ public/js/react_components/user/_calendar.jsx
@@ -5,6 +5,41 @@ define(['react', 'jquery', 'moment', 'fullcalendar', 'stores/CalendarStore'], fu
     },
     componentDidMount: function() {
       CalendarStore.addChangeListener(this._onChange);
+      // FullCalendar
+      $('#calendar').fullCalendar({
+        header: {
+          left: 'prev,next today',
+          center: 'title',
+          right: 'month,agendaWeek,agendaDay'
+        },
+        editable: true,
+        droppable: true,
+        drop: function() {
+          // is the "remove after drop" checkbox checked?
+           if ($('#drop-remove').is(':checked')) {
+            // if so, remove the element from the "Draggable Events" list
+            $(this).remove();
+           }
+         },
+        selectable: true,
+        selectHelper: true,
+        select: function(start, end) {
+          var title = prompt('Event Title:');
+          var eventData;
+          if (title) {
+            eventData = {
+              title: title,
+              start: start,
+              end: end
+            };
+            $('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
+          }
+          $('#calendar').fullCalendar('unselect');
+        },
+        editable: true,
+        eventLimit: true, // allow "more" link when too many events
+        events: this.state.events
+      });
     },
     _onChange: function() {
       this.setState(CalendarStore.getCurrentCal());
