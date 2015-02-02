a93e46fc0bce8791c0e5f5aa7f7d9a04025fe41f
achiu8 <achiu8@gmail.com>
2015-02-01 18:18:33 -0800

    Refactor EventAPI

--- public/js/utils/EventWebAPIUtils.js
+++ public/js/utils/EventWebAPIUtils.js
@@ -1,47 +1,27 @@
-define(['actions/EventServerActions', 'stores/UserStore'], function(EventServerActions, UserStore){
-
-	var EventAPI = {
-		retrieveUserEvents: function(){
-			var url = "/users/"+UserStore.currentUser() + "/events";
-			var that = this;
-			//Run once to load
-			$.ajax({
-			url: API_URL + url,
-			dataType: 'json'
-			}).done(function(data){
-				EventServerActions.loadAllEvents(data);
-			}).fail(function(data){
-				console.log("FAILED REQUEST");
-			});
-
-		},
-		createEvent: function(data) {
-			 $.ajax({
-			  url: API_URL + '/events',
-			  dataType: 'json',
-			  type: 'POST',
-			  data: data
-			}).success(function(data){
-				console.log(data);
-				// CalendarStore.updateCalById();
-			}).fail(function(data){
-			  console.log(data.statusText);
-			});
-		},
-		retrieveCalEvents: function(calendar_id) {
-			 $.ajax({
-			  url: API_URL + '/events',
-			  dataType: 'json',
-			  data: calendar_id
-			}).success(function(data){
-				console.log(data);
-			}).fail(function(data){
-			  console.log(data.statusText);
-			});
-		}
-
-	};
-
-	return EventAPI;	
-
+define([
+  'actions/EventServerActions',
+  'stores/UserStore'],
+  function(
+    EventServerActions,
+    UserStore
+  ) {
+  var EventAPI = {
+    retrieveUserEvents: function() {
+      //Run once to load
+      $.get(API_URL + "/users/" + UserStore.currentUser() + "/events")
+        .done(EventServerActions.loadAllEvents.bind(this))
+        .fail(function(data) { console.log("FAILED REQUEST"); }.bind(this));
+    },
+    createEvent: function(data) {
+      $.post(API_URL + '/events', data)
+        .done(function(data) { console.log(data); })
+        .fail(function(data) { console.log(data); });
+    },
+    retrieveCalEvents: function(calendar_id) {
+      $.get(API_URL + '/events', calendar_id)
+        .done(function(data){ console.log(data); })
+        .fail(function(data){ console.log(data.statusText); });
+    }
+  };
+  return EventAPI;
 });
