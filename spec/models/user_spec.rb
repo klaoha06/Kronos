7ccbb7e748efe973f9d3a7b5f10167c1f6898744
Peter Prakobkit <kprakobkit@gmail.com>
2015-02-04 11:16:44 -0800

    initial test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe User, :type => :model do
   describe '#get_future_events' do
     it "should return all future events for calendar user is following" do
+      p User.create(username: "hadchi", name: "haha", fb_id: "fb_id");
+      p User.first
     end
   end
 
