2c41e3729a2fe35ae3c12705ded4434b8002e95c
Peter Prakobkit <kprakobkit@gmail.com>
2015-02-04 11:31:12 -0800

    add congiruations for using databse cleaner

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,6 @@ require 'rails_helper'
 RSpec.describe User, :type => :model do
   describe '#get_future_events' do
     it "should return all future events for calendar user is following" do
-      p User.create(username: "hadchi", name: "haha", fb_id: "fb_id");
-      p User.first
     end
   end
 
