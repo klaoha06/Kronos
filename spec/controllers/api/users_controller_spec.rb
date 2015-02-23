bf75257368112758f325535d65afd7d3771fe394
achiu8 <achiu8@gmail.com>
2015-02-22 17:58:46 -0800

    Add users controller tests

--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Api::V0::UsersController do
   let(:user) { User.create(fb_id: 1) }
-  let(:auth) { Auth.create }
+  let(:auth) { Auth.create(user_id: 1, created_at: Time.now) }
 
   describe '#sessioning_user' do
-    it 'should update the current user if exists' do
+    it 'updates the current user if exists' do
       expect(User).to receive(:find_by).and_return(user)
       expect(user).to receive(:auth).twice.and_return(auth)
       expect(user).to receive(:update).and_return(true)
@@ -14,7 +14,7 @@ RSpec.describe Api::V0::UsersController do
       get :sessioning_user, { fb_id: 1 }
     end
 
-    it 'should create a new user if does not exist' do
+    it 'creates a new user if does not exist' do
       expect(User).to receive(:find_by).and_return(nil)
       expect(User).to receive(:new).and_return(user)
       expect(user).to receive(:save).and_return(true)
@@ -25,7 +25,60 @@ RSpec.describe Api::V0::UsersController do
 
   describe '#suggestions' do
     # need to change once actual algorithm for action is implemented
-    it 'should return all users not being followed' do
+    it 'returns all users not being followed' do
+      user2 = User.create(fb_id: 2)
+      user3 = User.create(fb_id: 3)
+      expect(User).to receive(:find).and_return(user)
+      expect(Auth).to receive(:find_by).and_return(auth)
+
+      get :suggestions
+      response_json = JSON.parse(response.body)
+      expect(response_json[0]['id']).to eq(user2.id)
+      expect(response_json[1]['id']).to eq(user3.id)
+    end
+  end
+
+  describe '#show' do
+    it 'shows user if followed or following' do
+      user4 = User.create(fb_id: 4)
+      user.following << user4
+      user.followers << user4
+      expect(User).to receive(:find).and_return(user)
+      expect(Auth).to receive(:find_by).and_return(auth)
+      expect(User).to receive(:find).and_return(user4)
+      get :show, id: user4.id
+      expect(response.body).to eq({
+        user: user4, friendship: { following: true, follower: true }
+      }.to_json)
+    end
+
+    it 'shows user if not followed or following' do
+      user4 = User.create(fb_id: 4)
+      expect(User).to receive(:find).and_return(user)
+      expect(Auth).to receive(:find_by).and_return(auth)
+      expect(User).to receive(:find).and_return(user4)
+      get :show, id: user4.id
+      expect(response.body).to eq({
+        user: user4, friendship: { following: false, follower: false }
+      }.to_json)
+    end
+  end
+
+  describe '#clear_session' do
+    it 'destroys auth object if exists' do
+      expect(User).to receive(:find).and_return(user)
+      expect(Auth).to receive(:find_by).and_return(auth)
+      expect(auth).to receive(:destroy!)
+      get :clear_session
+    end
+  end
+
+  describe '#friendships' do
+    it 'renders all followers and following for current user' do
+      expect(User).to receive(:find).twice.and_return(user)
+      expect(Auth).to receive(:find_by).and_return(auth)
+      get :friendships, id: 1
+      expect(response.body).to eq({ user: user, followers: [], following: [] }.to_json)
     end
   end
 end
